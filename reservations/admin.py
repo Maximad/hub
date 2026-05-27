2fcd4077d6f693009d07ab82875766056c2bedbc
Maximad <aboudiab@gmail.com>
2026-05-27 10:57:10 +0300

    Build Phase 7 staff events reservations and vendor operations

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Reservation
 
-admin.site.register(Reservation)
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'reservation_type', 'reservation_date', 'start_time', 'party_size', 'status', 'event', 'table_area')
+    list_filter = ('status', 'reservation_type', 'reservation_date', 'event')
+    search_fields = ('name', 'phone', 'notes')
