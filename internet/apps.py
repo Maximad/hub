57937d1e6a4cf90cd86e428dcc8bf8d9760f19b8
Maximad <aboudiab@gmail.com>
2026-08-22 11:18:39 +0300

    Load Internet catalog synchronization signals

--- internet/apps.py
+++ internet/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InternetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'internet'
+
+    def ready(self):
+        from . import signals  # noqa: F401
