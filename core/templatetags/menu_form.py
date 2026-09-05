0c207fb4a86fcec690b2c57bae39697f80ecde8a
Maximad <aboudiab@gmail.com>
2026-09-06 00:46:31 +0300

    Handle plain POS form mappings in modifier template tags

--- core/templatetags/menu_form.py
+++ core/templatetags/menu_form.py
@@ -10,6 +10,20 @@ def form_value(form_values, prefix, object_id, default=''):
     return form_values.get(f'{prefix}{object_id}', default)
 
 
+def _values_for(form_values, name):
+    """Return submitted values from QueryDicts and plain mapping fallbacks."""
+    getlist = getattr(form_values, 'getlist', None)
+    if callable(getlist):
+        return list(getlist(name))
+
+    value = form_values.get(name, [])
+    if value is None:
+        return []
+    if isinstance(value, (list, tuple, set)):
+        return list(value)
+    return [value]
+
+
 @register.simple_tag
 def option_selected(form_values, product_id, group_id, option_id, default=False):
     if not form_values:
@@ -17,5 +31,5 @@ def option_selected(form_values, product_id, group_id, option_id, default=False)
     names = [f'option_{product_id}_{group_id}', f'option_{product_id}_{group_id}[]']
     selected_values = []
     for name in names:
-        selected_values.extend(form_values.getlist(name))
-    return str(option_id) in selected_values
+        selected_values.extend(_values_for(form_values, name))
+    return str(option_id) in {str(value) for value in selected_values}
