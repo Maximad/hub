86a0b9f706e30e10612a104a2c7b60247ae9e118
Maximad <aboudiab@gmail.com>
2026-09-07 00:21:18 +0300

    Route passwordless member login

--- member_accounts/urls.py
+++ member_accounts/urls.py
@@ -5,6 +5,8 @@ from member_accounts import views
 
 urlpatterns = [
     path('join/<str:token>/', views.join_invitation, name='member_account_join'),
+    path('me/login/', views.member_login, name='member_account_login'),
+    path('me/login/<uuid:challenge_id>/', views.member_login_verify, name='member_account_login_verify'),
     path('me/', views.member_home, name='member_account_home'),
     path('me/logout/', views.member_logout, name='member_account_logout'),
     path('staff/member-invitations/new/', views.staff_invitation_new, name='staff_member_invitation_new'),
