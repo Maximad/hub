87c6a05566d257b4ecac700334e9ab014b2ef72f
Maximad <aboudiab@gmail.com>
2026-09-06 23:12:52 +0300

    Add member account app config

---
+++ member_accounts/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MemberAccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'member_accounts'
+    verbose_name = 'حسابات الأعضاء'
