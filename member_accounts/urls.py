65442ad5ff1a74890e5aee0bddcbffcf7899ece5
Maximad <aboudiab@gmail.com>
2026-09-07 14:04:25 +0300

    Route member card and staff scan endpoints

--- member_accounts/urls.py
+++ member_accounts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
-from member_accounts import views
+from member_accounts import card_views, views
 
 
 urlpatterns = [
     path('join/<str:token>/', views.join_invitation, name='member_account_join'),
     path('me/login/', views.member_login, name='member_account_login'),
     path('me/login/<uuid:challenge_id>/', views.member_login_verify, name='member_account_login_verify'),
+    path('me/card/qr.svg', card_views.member_card_qr, name='member_card_qr'),
     path('me/', views.member_home, name='member_account_home'),
     path('me/logout/', views.member_logout, name='member_account_logout'),
+    path('staff/member-card/<str:token>/', card_views.staff_member_card_scan, name='staff_member_card_scan'),
     path('staff/member-invitations/new/', views.staff_invitation_new, name='staff_member_invitation_new'),
     path('staff/members/<uuid:member_id>/invite/', views.staff_invitation_new, name='staff_member_invitation_existing'),
     path('staff/members/<uuid:member_id>/account/lock/', views.staff_lock_account, name='staff_member_account_lock'),
