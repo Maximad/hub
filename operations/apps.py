938f1ddd2e0b0993200c54202a9077c54342d7cb
Maximad <aboudiab@gmail.com>
2026-09-22 13:08:07 +0300

    Register operations notification safeguards

--- operations/apps.py
+++ operations/apps.py
@@ -5,3 +5,6 @@ class OperationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'operations'
     verbose_name = 'اليوم التشغيلي'
+
+    def ready(self):
+        from . import signals  # noqa: F401
